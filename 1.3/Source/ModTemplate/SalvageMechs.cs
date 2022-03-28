8580eca49aceb3e195d360411fd6d29fa28082db
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2022-03-28 15:56:38 +0200

    TRied and failed to make shields

--- 1.3/Source/ModTemplate/SalvageMechs.cs
+++ 1.3/Source/ModTemplate/SalvageMechs.cs
@@ -4,17 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RimWorld;
+using UnityEngine;
 using Verse;
-using HarmonyLib;
 
-namespace MT
+namespace SMC
 {
     [StaticConstructorOnStartup]
-    public static class ModTemplate
+    public class SMC_CompProjectileInterceptor : ThingComp
     {
-        static ModTemplate()
+        public SMC_CompProperties_ProjectileInterceptor Props
         {
+            get
+            {
+                return (SMC_CompProperties_ProjectileInterceptor)this.props;
+            }
+        }
+    }
+    public class SMC_CompProperties_ProjectileInterceptor : CompProperties
+    {
+        public SMC_CompProperties_ProjectileInterceptor()
+        {
+            this.compClass = typeof(SMC_CompProjectileInterceptor);
 
         }
+
+        public float radius;
+
+        public int disarmedByEmpForTicks;
+
+        public bool interceptGroundProjectiles;
+
+        public bool interceptAirProjectiles;
+
+        public int chargeDurationTicks;
+
+        public Color color = Color.white;
+
+        public EffecterDef reactivateEffect;
+
+        public EffecterDef interceptEffect;
+
+        public SoundDef activeSound;
     }
 }
